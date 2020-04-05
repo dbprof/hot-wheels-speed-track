169c8c3d2080ab1a6a15d3de00ab9da5eb578881
BlackBug Engineering <dbprof@yandex.ru>
2020-04-05 22:23:36 +0300

    added rgb leds

--- HotWheelsSpeedTrack.ino
+++ HotWheelsSpeedTrack.ino
@@ -1,46 +1,68 @@
-
 /**************************************************************
 BlackBug Engineering
-25.01.2019
+02.04.2020
 https://github.com/dbprof/hot-wheels-speed-track
 ***************************************************************/
-//Библиотека сервопривода
+// Библиотека сервопривода
 #include <Servo.h> 
 
-//Библиотека ЖК-дисплея
+// Библиотека ЖК-дисплея
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 
+// Определение пинов инфракрасных датчиков 
 #define IR1_PIN 2
 #define IR2_PIN 3
+
+// Определение пина кнопки
 #define BTN_PIN 8
+
+// Определение пинов сервоприводов
 #define SERVO1_PIN 9
 #define SERVO2_PIN 10
+
+// Определение пина пищалки
 #define BUZZER_PIN 12
 
-//Объявляем переменные времени
+// Определение углов поворота сервоприводов (определяются опытным путем)
+// позволяют проводить дополнительную юстировку в случае не одновременного срабатывания
+#define L_SERV_ANGLE_CLOS 153 // Угол левого сервопривода в закрытом состоянии
+#define R_SERV_ANGLE_CLOS 6   // Угол правого сервопривода в закрытом состоянии
+#define L_SERV_ANGLE_OPEN 43  // Угол левого сервопривода в открытом состоянии
+#define R_SERV_ANGLE_OPEN 96  // Угол правого сервопривода в открытом состоянии
+
+// Определение пинов светодиодов 
+#define RED1 4
+#define GREEN1 5
+#define RED2 6
+#define GREEN2 7
+
+// Объявляем переменные времени
 uint32_t uiStartTime, uiFinishTime1, uiFinishTime2;
 
-//Объявляем переменную расстояния и подставляем длину трассы в метрах
-double dDistance = 0.5; //метров
+// Объявляем переменную расстояния и подставляем длину трассы в метрах
+double dDistance = 0.5; //метров (определяется замером)
 
-//Объявляем переменную скорости
+// Объявляем переменные скорости
 double dSpeed1, dSpeed2;
 
-//Объявляем переменную сигнала ИК-датчика
+// Объявляем переменные сигнала ИК-датчика
 byte bIR1Signal, bIR2Signal;
 
-//Объявляем ЖК-дисплей
+// Объявляем ЖК-дисплей
 LiquidCrystal_I2C lcd(0x27,16,2);
 
-//Объявляем сервоприводы
+// Объявляем сервоприводы
 Servo ServoLeft;
 Servo ServoRight;
 
-//Объявляем признак подключения сервопривода
+// Объявляем углы поворота сервопривода
+int iLeftServoAngle, iRightServoAngle;
+
+// Объявляем признак подключения сервопривода
 bool isServoAttached = false;
 
-//Подпрограмма подключения сервопривода
+// Подпрограмма подключения сервопривода
 void attachServo() {
   if (!isServoAttached) {
     ServoLeft.attach(SERVO1_PIN);
@@ -48,7 +70,8 @@ void attachServo() {
     isServoAttached = true;
   }
 }
-//Подпрограмма отключения сервопривода
+
+// Подпрограмма отключения сервопривода
 void detachServo() {
   if (isServoAttached) {
     ServoLeft.detach();
@@ -56,24 +79,37 @@ void detachServo() {
     isServoAttached = false;
   }
 }
-//Подпрограмма закрытия барьера
+
+// Подпрограмма закрытия барьера
 void closeGate() {
   attachServo();
-  ServoLeft.write(155);
-  ServoRight.write(10);
-  delay(300);
-  detachServo();
-}
-//Подпрограмма открытия барьера
-void openGate() {
-  attachServo();
-  ServoLeft.write(35);
-  ServoRight.write(130);
+  ServoLeft.write(L_SERV_ANGLE_CLOS);
+  ServoRight.write(R_SERV_ANGLE_CLOS);
   delay(300);
   detachServo();
 }
 
-//Функция вывода на ЖК-дисплей
+// Подпрограмма открытия барьера
+void openGate() {
+  attachServo();
+  ServoLeft.write(L_SERV_ANGLE_OPEN);
+  ServoRight.write(R_SERV_ANGLE_OPEN);
+  delay(300);
+  detachServo();
+}
+
+// Подпрограмма переводящая барьер в положение по умолчанию
+void defaultGate() {
+  attachServo();
+  ServoLeft.write(L_SERV_ANGLE_CLOS);
+  ServoRight.write(R_SERV_ANGLE_CLOS);
+  delay(300);
+  iLeftServoAngle = ServoLeft.read();
+  iRightServoAngle = ServoRight.read();
+  detachServo();
+}
+
+// Функция вывода на ЖК-дисплей
 void printLCD(String stVar1, String stVar2){
   lcd.clear();  
   lcd.setCursor(0, 0); //первая строка
@@ -82,36 +118,58 @@ void printLCD(String stVar1, String stVar2){
   lcd.print(stVar2);
 }
 
+// Функция обнуления светодиодов - все не светятся
+void ledreset() {
+  digitalWrite(RED1, 1);
+  digitalWrite(GREEN1, 1);
+  digitalWrite(RED2, 1);
+  digitalWrite(GREEN2, 1);
+}
+
+// Функция обнуления переменных
 void clearAll(){
   uiStartTime = 0;
   uiFinishTime1 = 0;
   uiFinishTime2 = 0;
   dSpeed1 = 0.0;
   dSpeed2 = 0.0;
+  ledreset();
 }
 
-
 void setup() {
+  // Установка вывода для тестирования
+  //Serial.begin(9600);
+
+  // Установка светодиодов
+  pinMode(RED1, OUTPUT);
+  pinMode(GREEN1, OUTPUT);
+  pinMode(RED2, OUTPUT);
+  pinMode(GREEN2, OUTPUT);
+
+  // Установка инфракрасных датчиков
   pinMode(IR1_PIN, INPUT);
   pinMode(IR2_PIN, INPUT);
+
+  // Установка кнопки
   pinMode(BTN_PIN, INPUT);
-  //pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(4, OUTPUT);
+
+  // Установка пищалки
   pinMode(BUZZER_PIN,OUTPUT);
   
-  closeGate();
+  // Установка барьера в начальное положение
+  defaultGate();
   
-  //Инициализация ЖК-дисплея
-  lcd.init();
-  //Включаем подсветку                     
-  lcd.backlight();
-  //Обнуляем переменные
+  // Инициализация ЖК-дисплея
+  lcd.init();      
+  lcd.backlight(); //Включаем подсветку
+  
+  // Обнуляем переменные
   clearAll();
 }
 
 void loop() {
   
-  //Проверка датчиков
+  // Проверка датчиков
   bIR1Signal = digitalRead( IR1_PIN );
   bIR2Signal = digitalRead( IR2_PIN );
   if (bIR1Signal == LOW && bIR2Signal == LOW){
@@ -127,12 +185,13 @@ void loop() {
       printLCD("GO!!!","");
       tone(BUZZER_PIN,1500,300);
 
-      //Открываем трассу
+      // Открываем трассу
       openGate();
-      //Засекаем время
+      
+      // Засекаем время
       uiStartTime = millis();
       
-      //Ждем срабатывания обоих датчиков и расчитываем время
+      // Ждем срабатывания обоих датчиков и расчитываем время
       do {
         bIR1Signal = digitalRead( IR1_PIN );
         bIR2Signal = digitalRead( IR2_PIN );
@@ -140,31 +199,55 @@ void loop() {
         if (bIR2Signal == HIGH && uiFinishTime2 == 0) uiFinishTime2 = millis() - uiStartTime;
       }
       while (uiFinishTime1 == 0 || uiFinishTime2 == 0);
-    
+
+      // Играем звук завершения замера
       tone(BUZZER_PIN,3000,500);
       delay(500);
+      
       //Закрываем трассу
       closeGate();
 
-      //Расчитываем скорость
+      // Расчитываем скорость
       dSpeed1 = dDistance/uiFinishTime1*1000;  //скорость = м/с
       dSpeed2 = dDistance/uiFinishTime2*1000;  //скорость = м/с
 
-      //Выводим значения на дисплей до нажатия кнопки
-      printLCD(String(uiFinishTime1) + " ms " + String(dSpeed1) + " m/s",
-               String(uiFinishTime2) + " ms " + String(dSpeed2) + " m/s");
+      // Выводим значения на дисплей до нажатия кнопки
+      printLCD("1:" + String(uiFinishTime1) + "ms " + String(dSpeed1) + "m/s",
+               "2:" + String(uiFinishTime2) + "ms " + String(dSpeed2) + "m/s");
+
+      // Зажигаем светодиодные индикаторы в зависимости от результата
+      if (dSpeed1 > dSpeed2) {
+        digitalWrite(GREEN1, 0);
+        digitalWrite(RED2, 0);
+      }
+      else if (dSpeed1 < dSpeed2){
+        digitalWrite(RED1, 0);
+        digitalWrite(GREEN2, 0);
+      }
+      else {
+        digitalWrite(GREEN1, 0);
+        digitalWrite(GREEN2, 0);
+      }
+
+      // Ждем нажатия кнопки для повторного замера
       while (digitalRead(BTN_PIN) == LOW){};
       clearAll();
     }
   }
+
+  // Продолжени проверки датчиков - не работает датчик 2
   else if(bIR1Signal == LOW && bIR2Signal == HIGH){
     printLCD("IR Sensor Error","CHECK SIGNAL 2");
     delay(200);
   }
+
+  // Продолжени проверки датчиков - не работает датчик 1
   else if(bIR1Signal == HIGH && bIR2Signal == LOW){
     printLCD("IR Sensor Error","CHECK SIGNAL 1");
     delay(200);
   }
+
+  // Продолжени проверки датчиков -  - не работают датчики 1 и 2
   else{
     printLCD("IR Sensor Error","CHECK SIGNAL 1,2");
     delay(200);
